d88a55fa29bb0b247fcc561ceb2720cd59fbff2b
halimegldn <gildanhalime60@gmail.com>
2025-06-14 17:15:55 +0300

    İlk commit

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <div>Stajyer Takip Sistemi</div>
+  );
+}
---
+++ prisma/migrations/20250525114020_first_model_added/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Stajyer" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
+
+    CONSTRAINT "Stajyer_pkey" PRIMARY KEY ("id")
+);
